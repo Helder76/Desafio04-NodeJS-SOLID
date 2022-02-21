b3e6c9180fbb6f244f50861a2286546771c4fde8
Helder76 <freitas.h@gmail.com>
2022-02-21 11:41:55 -0300

    Final test

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("User without permission");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
